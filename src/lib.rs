19ea2eb91b8dbbb60453082e7f9182a3012a8de7
@thorning_m <matthewthorning@gmail.com>
2019-05-19 19:58:01 +0100

    refactored into lib file and added error handling

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use serde::{Deserialize, Serialize};
+use std::cell::Cell;
+
+#[derive(Deserialize, Serialize)]
+struct TrackData {
+    channel: String,
+    payload: Payload,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Payload {
+    title: String,
+    artist: String,
+    album: String,
+}
+
+struct NowPlaying {
+    title: String,
+    artist: String,
+}
+
+impl NowPlaying {
+    fn from(track_data: TrackData) -> NowPlaying {
+        NowPlaying {
+            title: track_data.payload.title,
+            artist: track_data.payload.artist,
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct PlayState {
+    payload: bool,
+}
+
+pub struct Playing {
+    pub is_playing: Cell<bool>,
+}
+
+impl Playing {
+    pub fn new() -> Playing {
+        Playing {
+            is_playing: Cell::new(false),
+        }
+    }
+    pub fn set_playing(&self, message: String) {
+        let play_state: PlayState = serde_json::from_str(&message[..]).unwrap();
+        self.is_playing.set(play_state.payload);
+    }
+    pub fn get_now_playing(message: String, playing: bool) {
+        if playing {
+            let track_data: TrackData = serde_json::from_str(&message[..]).unwrap();
+            let now_playing = NowPlaying::from(track_data);
+            println!("\u{1F3B6}{} by {}", now_playing.title, now_playing.artist);
+        } else {
+            println!("\u{1F3B6}Nothing playing at the moment");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,38 @@
-use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::cell::Cell;
-use ws::Message;
-use ws::{connect, CloseCode};
+use ws::{connect, CloseCode, Error, Handler, Message, Result, Sender};
 
-#[derive(Deserialize, Serialize)]
-struct TrackData {
-    channel: String,
-    payload: Payload,
+use now_playing::Playing;
+
+struct Client {
+    out: Sender,
+    playing: Playing,
 }
 
-#[derive(Deserialize, Serialize)]
-struct Payload {
-    title: String,
-    artist: String,
-    album: String,
-}
+impl Handler for Client {
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        match msg {
+            Message::Text(message) => {
+                let data: Value = serde_json::from_str(&message[..]).unwrap();
+                if data["channel"] == "playState" {
+                    self.playing.set_playing(message);
+                } else if data["channel"] == "track" {
+                    Playing::get_now_playing(message, self.playing.is_playing.get());
+                }
+            }
+            _ => println!(),
+        };
 
-struct NowPlaying {
-    title: String,
-    artist: String,
-}
-
-impl NowPlaying {
-    fn from(track_data: TrackData) -> NowPlaying {
-        NowPlaying {
-            title: track_data.payload.title,
-            artist: track_data.payload.artist,
-        }
+        self.out.close(CloseCode::Normal)
     }
-}
-
-#[derive(Serialize, Deserialize)]
-struct PlayState {
-    payload: bool,
-}
-
-struct Playing {
-    is_playing: Cell<bool>,
-}
-
-impl Playing {
-    fn new() -> Playing {
-        Playing {
-            is_playing: Cell::new(false),
-        }
-    }
-    fn set_playing(&self, message: String) {
-        let play_state: PlayState = serde_json::from_str(&message[..]).unwrap();
-        self.is_playing.set(play_state.payload);
-    }
-}
-
-fn get_now_playing(message: String, playing: bool) {
-    if playing {
-        let track_data: TrackData = serde_json::from_str(&message[..]).unwrap();
-        let now_playing = NowPlaying::from(track_data);
-        println!("\u{1F3B6}{} by {}", now_playing.title, now_playing.artist);
-    } else {
-        println!("\u{1F3B6}The Sound of Silence");
+    fn on_error(&mut self, _err: Error) {
+        println!("\u{1F3B6}GPMDP isn't running");
     }
 }
 
 fn main() {
     connect("ws://localhost:5672", |out| {
         let playing = Playing::new();
-        move |msg: Message| {
-            match msg {
-                Message::Text(message) => {
-                    let data: Value = serde_json::from_str(&message[..]).unwrap();
-                    if data["channel"] == "playState" {
-                        playing.set_playing(message);
-                    } else if data["channel"] == "track" {
-                        get_now_playing(message, playing.is_playing.get());
-                    }
-                }
-                _ => (),
-            };
-
-            out.close(CloseCode::Normal)
-        }
+        Client { out, playing }
     })
     .unwrap();
 }
