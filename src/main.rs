71ba6e99d40390df8ef01b2e0178834b6473c72c
@thorning_m <matthewthorning@gmail.com>
2019-05-19 17:23:07 +0100

    show "The Sound of Silence" if nothing is playing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::cell::Cell;
 use ws::Message;
 use ws::{connect, CloseCode};
 
@@ -30,22 +31,48 @@ impl NowPlaying {
     }
 }
 
-fn get_now_playing(message: String) {
-    let track_data: TrackData = serde_json::from_str(&message[..]).unwrap();
-    let now_playing = NowPlaying::from(track_data);
-    println!("\u{1F3B6}{} by {}", now_playing.title, now_playing.artist);
+#[derive(Serialize, Deserialize)]
+struct PlayState {
+    payload: bool,
+}
+
+struct Playing {
+    is_playing: Cell<bool>,
+}
+
+impl Playing {
+    fn new() -> Playing {
+        Playing {
+            is_playing: Cell::new(false),
+        }
+    }
+    fn set_playing(&self, message: String) {
+        let play_state: PlayState = serde_json::from_str(&message[..]).unwrap();
+        self.is_playing.set(play_state.payload);
+    }
+}
+
+fn get_now_playing(message: String, playing: bool) {
+    if playing {
+        let track_data: TrackData = serde_json::from_str(&message[..]).unwrap();
+        let now_playing = NowPlaying::from(track_data);
+        println!("\u{1F3B6}{} by {}", now_playing.title, now_playing.artist);
+    } else {
+        println!("\u{1F3B6}The Sound of Silence");
+    }
 }
 
 fn main() {
     connect("ws://localhost:5672", |out| {
-        out.send("Hello WebSocket").unwrap();
-
+        let playing = Playing::new();
         move |msg: Message| {
             match msg {
                 Message::Text(message) => {
                     let data: Value = serde_json::from_str(&message[..]).unwrap();
-                    if data["channel"] == "track" {
-                        get_now_playing(message);
+                    if data["channel"] == "playState" {
+                        playing.set_playing(message);
+                    } else if data["channel"] == "track" {
+                        get_now_playing(message, playing.is_playing.get());
                     }
                 }
                 _ => (),
